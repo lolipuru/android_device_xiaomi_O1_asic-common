238066db54e46bff5123ffbb054be87f8b2dcfde
lolipuru <banstellazz@gmail.com>
2026-02-09 13:43:59 +0000

    o1_asic-common: [WIP]: intiial proprietary-files
    Change-Id: I7f37cb8b48389a6d4054ee403c15c013c49fa25c
    Signed-off-by: lolipuru <banstellazz@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+import extract_utils.tools
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/o1_asic-common',
+    'hardware/mediatek',
+    'hardware/xiaomi',
+    'vendor/xiaomi/o1_asic-common',
+]
+
+blob_fixups: blob_fixups_user_type = {
+}
+
+module = ExtractUtilsModule(
+    'o1_asic-common',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+    add_firmware_proprietary_file=False,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
