3255094dd4b0d8c1981df161f33ac975031153b4
lolipuru <banstellazz@gmail.com>
2026-02-09 21:00:35 +0000

    o1_asic-common: import more prebuilts
    Change-Id: I9b152748cac8289cfa89f3be31eb7a956170e27c
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -20,7 +20,6 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/o1_asic-common',
     'hardware/mediatek',
     'hardware/xiaomi',
     'vendor/xiaomi/o1_asic-common',
@@ -29,12 +28,81 @@ namespace_imports = [
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
+        'libmialgo',
         'libcamlog',
         'com.xiaomi.camdfx'
     ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
+    (
+        'system_ext/lib64/libimsma.so',
+    ): blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'vendor/bin/hw/android.hardware.media.c2@1.2-service-x': blob_fixup()
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v35.so')
+        .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v35.so')
+        .replace_needed('libcodec2_hidl@1.2.so', 'libcodec2_hidl@1.2-v35.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v35.so'),
+    'vendor/lib64/libcodec2_hidl@1.0-v35.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v35.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v35.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v35.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+    'vendor/lib64/libcodec2_hidl@1.1-v35.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v35.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v35.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v35.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v35.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+    'vendor/lib64/libcodec2_hidl@1.2-v35.so': blob_fixup()
+        .replace_needed('libstagefright_bufferqueue_helper.so', 'libstagefright_bufferqueue_helper-v35.so')
+        .replace_needed('libcodec2_hidl@1.0.so', 'libcodec2_hidl@1.0-v35.so')
+        .replace_needed('libcodec2_hidl@1.1.so', 'libcodec2_hidl@1.1-v35.so')
+        .replace_needed('libcodec2_hidl_plugin.so', 'libcodec2_hidl_plugin-v35.so')
+        .replace_needed('libcodec2_vndk.so', 'libcodec2_vndk-v35.so')
+        .replace_needed('libui.so', 'libui-v34.so'),
+    (
+        'vendor/lib64/hw/android.hardware.graphics.allocator-V2-xring.so',
+        'vendor/lib64/libjpeg_x_sdk.so',
+        'vendor/lib64/libjpeg_x_server.so',
+        'vendor/lib64/vendor.xring.graphics.gcb-V1-ndk.so',
+        'vendor/lib64/vendor.xring.hardware.display.composerext-V1-ndk.so',
+        'vendor/lib64/hw/mapper.xring.so',
+    ): blob_fixup()
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
+    (
+        'vendor/lib64/libcodec2_x_vdec.so',
+        'vendor/lib64/libcodec2_x_venc.so',
+    ): blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so')
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
+    (
+        'odm/bin/hw/vendor.xiaomi.sensor.citsensorservice.aidl',
+        'vendor/lib64/libcodec2_x_c2store.so',
+    ): blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so')
+        .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
+    (
+        'odm/lib64/hw/displayfeature.default.so',
+        'odm/lib64/libadaptivehdr.so',
+        'odm/lib64/libcolortempmode.so',
+        'odm/lib64/libdither.so',
+        'odm/lib64/libflatmode.so',
+        'odm/lib64/libhistprocess.so',
+        'odm/lib64/libmiBrightness.so',
+        'odm/lib64/libmiSensorCtrl.so',
+        'odm/lib64/libpaperMode.so',
+        'odm/lib64/librhytheyecare.so',
+        'odm/lib64/libsdr2hdr.so',
+        'odm/lib64/libsre.so',
+        'odm/lib64/libtruetone.so',
+        'odm/lib64/libvideomode.so',
+        'vendor/bin/hw/android.hardware.graphics.composer3-service.xring',
+        'vendor/lib64/libxdmcore.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
+
     (
         'odm/lib64/libmt_mitee.so',
         'vendor/bin/hw/android.hardware.security.keymint@3.0-service.mitee'
